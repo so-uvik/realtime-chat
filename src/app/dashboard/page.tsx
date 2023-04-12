ce56d560a0d780a4250c32b36ae3da948ca442fb
souviksaha27 <souviksahamay27@gmail.com>
2023-04-12 13:39:32 +0530

    setup the project and added helper functions

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button />
+}
+
+export default page
